fa1a0e2aee13b18bc53e80c5dd1db48a6dfcfd2b
Marc <sosatony111@gmail.com>
2024-10-19 14:06:14 -0500

    added a submit_form

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -9,3 +9,9 @@ def home(request):
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", { "todos":items})
+
+def submit_form(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('fname') #this saves the fname in the html file to first_name
+        last_name = request.POST.get('lname') #same but for last name
+        print(f'First Name: {first_name}, Last Name: {last_name}')
