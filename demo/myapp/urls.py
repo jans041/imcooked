fa075486aede1eda0343dd856a99b3349c064169
jans041 <ohimactuallyjan@gmail.com>
2024-10-19 19:06:30 -0500

    implement buttons

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("todos/", views.todos, name="Todos")
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact")
 ]
 
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -6,12 +6,8 @@ from .models import TodoItem
 def home(request):
     return render(request, "home.html") 
 
-def todos(request):
-    items = TodoItem.objects.all()
-    return render(request, "todos.html", { "todos":items})
+def about(request):
+    return render(request, 'about.html')
 
-def submit_form(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('fname') #this saves the fname in the html file to first_name
-        last_name = request.POST.get('lname') #same but for last name
-        print(f'First Name: {first_name}, Last Name: {last_name}')
+def contact(request):
+    return render(request, 'contact.html')
