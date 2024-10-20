27506d2ff4069eade7ad2eb667b30cd7b5cfe946
Marc <sosatony111@gmail.com>
2024-10-20 02:22:34 -0500

    fixed issue in setting.py it didnt have the ability to add images

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILE_DIRS = [
+    BASE_DIR / 'static',
+    "var/www/static/"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
